5a8e693f408ca9a6273eddb951afcf1e802ce65f
Mikolaj Dzieciolowski <i273080@lab137-02.stud.ii>
2017-11-09 13:00:50 +0100

    zad1-1

--- zadanie1.m
+++ zadanie1.m
@@ -1,7 +1,7 @@
 R1 = rand(10000);
-h=hist(R1, 100);
-imwrite(h, 'hist1zad1.png');
+h=hist(R1, 100)
+imwrite(h, 'hist1zad1.png')
 
 R2 = rand(1000);
-h2=hist(R2, 100);
-imwrite(h2, 'hist2zad1.png');
+h2=hist(R2, 100)
+imwrite(h2, 'hist2zad1.png')
